f069219f5facfd3a921c4d276a9869000eced2e9
AminBouchareb <93881530+boucharebea@users.noreply.github.com>
2023-12-03 18:45:21 +0100

    addmin main menu

--- GameClient/src/Default/Client.java
+++ GameClient/src/Default/Client.java
@@ -5,17 +5,53 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private static final String SERVER_ADDRESS = "127.0.0.1";
     private static final int SERVER_PORT = 2020;
 
 	public static void main(String[] args) {
-		String message="";
+		System.out.println("guess number between 0-10 ");
+		try {
+			doGuess();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+		
 
 	}
 
-    private static void doSend(Socket socket, String message) throws IOException {
+    private static void doGuess() throws IOException {
+		System.out.println(">> ");
+		int message=new Scanner(System.in).nextInt();
+		Socket socket=new Socket(SERVER_ADDRESS,SERVER_PORT);
+		doSendInt(socket, message);
+		
+		int receivedMessage=doReceiveInt(socket);
+		displayMessage(receivedMessage);
+		
+		
+	}
+
+
+
+	private static void displayMessage(int rm) throws IOException {
+		if(rm==0) {
+			System.out.println("try again");
+			doGuess();
+		}
+		if(rm==1) {
+			System.out.println("you losed ");
+		}
+		
+		
+		
+	}
+
+	private static void doSend(Socket socket, String message) throws IOException {
         DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
         outToServer.writeBytes(message + "\n");
     }
